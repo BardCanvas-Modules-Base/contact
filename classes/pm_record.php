63c63c713d359c8b6533519ce84f25696b0dbafa
Alejandro Caballero <lava.caballero@gmail.com>
2016-09-05 03:02:30 -0500

    Added contents processor

--- classes/pm_record.php
+++ classes/pm_record.php
@@ -19,4 +19,21 @@ class pm_record extends abstract_record
     {
         $this->id_pm = uniqid();
     }
+    
+    public function get_processed_contents()
+    {
+        global $config, $modules;
+        
+        $contents = nl2br($this->contents);
+        $contents = convert_emojis($contents);
+        
+        $contents = convert_media_tags($contents);
+        $contents = autolink_hash_tags($contents, "{$config->full_root_path}/tag/");
+        
+        $config->globals["processing_contents"] = $contents;
+        $modules["contact"]->load_extensions("pm_record_class", "get_processed_contents");
+        $contents = $config->globals["processing_contents"];
+        
+        return $contents;
+    }
 }
